8945f436d125ce3cadf5434535bc1aa4e980daa5
fg12345678901 <158485148+fg12345678901@users.noreply.github.com>
2025-07-11 20:41:45 +0800

    Update loop_mult.sh

--- loop_mult.sh
+++ loop_mult.sh
@@ -4,10 +4,11 @@ trap 'echo "[!] Caught SIGINT – killing children"; kill 0; exit 130' INT TERM
 
 while true; do
   # ------- 并行 self-play -------
-  bash utils/selfplay_parallel.sh 300 0 1 2 3
+  bash utils/selfplay_parallel.sh 1000 0 1 2 3
 
   # ------- DataParallel训练 -------
-  CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py train --updates 4000
+  CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py train 
+  # 不单独给 --updates 参数了，统一在config里
 
   # ------- 并行 Arena -------
   bash utils/eval_parallel.sh 104 0 1 2 3
